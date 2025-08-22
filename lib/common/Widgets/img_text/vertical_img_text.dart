250d7ee27b7e3b7e35b6ebde76a662ac8c3ef185
Kajal Maurya <kajalmaurya3366@gmail.com>
2025-08-22 17:11:37 +0530

    home category

---
+++ lib/common/Widgets/img_text/vertical_img_text.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constant/colors.dart';
+import '../../../utils/constant/sizes.dart';
+import '../custome_shape/clipper/circular_container.dart';
+
+class KVerticalImageText extends StatelessWidget {
+  const KVerticalImageText({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.textColor,
+    this.backgroundColor,
+    this.onTap,
+  });
+
+  final String image, title;
+  final Color textColor;
+  final Color? backgroundColor;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          // ----------- Circular Image ---------------
+          KCircularContainer(
+            height: 56,
+            width: 56,
+            bgColor: backgroundColor ?? KColors.white,
+            padding: EdgeInsets.all(KSizes.sm),
+            child: Image(image: AssetImage(image), fit: BoxFit.cover),
+          ),
+          SizedBox(height: KSizes.spaceBtwItems / 2),
+
+          // -------------- Title --------------------
+          SizedBox(
+            width: 55,
+            child: Text(
+              title,
+              style: Theme.of(
+                context,
+              ).textTheme.labelMedium!.apply(color: textColor),
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
